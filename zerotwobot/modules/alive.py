9ea21d4d68dbb58d401326737c1a386159c666d0
Axelrexkun <86648749+Axelrexkun@users.noreply.github.com>
2022-05-23 10:32:36 +0530

    Update alive.py

--- zerotwobot/modules/alive.py
+++ zerotwobot/modules/alive.py
@@ -15,10 +15,10 @@ PHOTO ="https://telegra.ph/file/6fa5f235ded76e554249b.jpg"
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Luffy. The Sun God.** \n\n"
   TEXT += "❍ **I'm Working Properly** \n\n"
-  TEXT += f"❍ **My Master : [Axel](https://t.me/YuitoSan)** \n\n"
+  TEXT += f"❍ **My Sensei : [Axel](https://t.me/YuitoSan)** \n\n"
   TEXT += f"❍ **Library Version :** `{telever}` \n\n"
   TEXT += f"❍ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"❍ **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 ✉️", "https://t.me/https://t.me/NikaSupportChat"), Button.url("Chat Group 📡", "https://t.me/animeuniversediscusion")]]
+  TEXT += "**Thanks For Adding Me Here**"
+  BUTTON = [[Button.url("Support", "https://t.me/https://t.me/NikaSupportChat"), Button.url("Chat Group", "https://t.me/animeuniversediscusion")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
