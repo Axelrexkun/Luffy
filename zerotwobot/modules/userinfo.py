eed0e7c8fe67baf1d65f386ba617f2f914db2e53
Axel <86648749+Axelrexkun@users.noreply.github.com>
2022-06-04 00:18:58 +0530

    Update userinfo.py

--- zerotwobot/modules/userinfo.py
+++ zerotwobot/modules/userinfo.py
@@ -14,7 +14,7 @@ from telegram.ext.dispatcher import run_async
 from telegram.error import BadRequest
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from Mizuhararobot import (
+from zerotworobot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
